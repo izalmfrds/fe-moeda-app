c3fc2f89ee605b3b397d02c20f485f64abba2376
izalmfrds <67822136+izalmfrds@users.noreply.github.com>
2026-02-08 12:38:33 +0700

    style button product card

--- components/ui/ProductCard/index.tsx
+++ components/ui/ProductCard/index.tsx
@@ -39,14 +39,18 @@ export default function Card({
       {/* Content Section */}
       <div className="p-2 flex flex-col flex-1">
         <div className="flex-1">
-          <h3 className="text-sm font-semibold text-gray-900 line-clamp-2">{title}</h3>
-          <p className="text-xs text-neutral-400 mt-1 font-normal line-clamp-2">{description}</p>
+          <h3 className="text-sm font-semibold text-gray-900 line-clamp-2">
+            {title}
+          </h3>
+          <p className="text-xs text-neutral-400 mt-1 font-normal line-clamp-2">
+            {description}
+          </p>
           {price && <p>{formatToIDR(price)}</p>}
         </div>
 
         {/* Button - Always at bottom */}
         <button
-          className="mt-4 w-full flex items-center justify-center gap-2 bg-primary-500 hover:bg-primary-800 text-white text-sm font-semibold py-1 rounded-full flex-shrink-0"
+          className="mt-4 w-full flex items-center justify-center gap-2 bg-primary-500 hover:bg-primary-800 text-white text-sm font-semibold py-1 rounded-[8px] flex-shrink-0"
           onClick={onAddToCart}
         >
           <span className="text-lg">+</span>
